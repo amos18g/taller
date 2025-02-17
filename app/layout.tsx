1ff503f60eefb915bbf85ccb6ecdebe513588042
jdg03 <gonzalez5.jose52@gmail.com>
2025-02-16 19:24:08 -0600

    editando las paginas de login y registro 3

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,8 @@
-//import DeployButton from "@/components/deploy-button";
-//import { EnvVarWarning } from "@/components/env-var-warning";
-//import HeaderAuth from "@/components/header-auth";
-//import { ThemeSwitcher } from "@/components/theme-switcher";
-//import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 import { Geist } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import "./globals.css";
 import { Geist_Mono } from "next/font/google";
-//import Link from "next/link";
-import "./globals.css";
-
-import Footer  from "@/components/Footer/Footer";
+import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
 
 const geistSans = Geist({
@@ -33,34 +25,34 @@ export const metadata = {
   description: "Administrador de taller",
 };
 
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-
     <html lang="en" className={geistSans.className} suppressHydrationWarning>
-    <body className="bg-background text-foreground">
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
-      >
-        <main className="min-h-screen flex flex-col items-center">
-          <div className="flex-1 w-full flex flex-col gap-20 items-center">
-            <Header />
-            <div className="flex flex-col gap-1 max-w-10xl p-0 mt-[-70]">
-              {children}
-            </div>
+      <body className="bg-background text-foreground">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Header />
+          <div className="min-h-screen flex flex-col">
+            
+            <main className="flex-1 flex flex-col items-center">
+              <div className="w-full flex flex-col gap-20 items-center">
+                <div className="flex flex-col gap-1 max-w-10xl p-0">
+                  {children}
+                </div>
+              </div>
+            </main>
+            <Footer />
           </div>
-        </main>
-        <Footer />
-      </ThemeProvider>
-    </body>
-  </html>
-
+        </ThemeProvider>
+      </body>
+    </html>
   );
 }
