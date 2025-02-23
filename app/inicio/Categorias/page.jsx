311ab105815ef8fb5f0d3c275da10816609eb919
JackeVillalobos <jackelinevillalobos11@gmail.com>
2025-02-23 00:40:58 -0600

    Actualiza estilos y lógica del CRUD Categorias

--- app/inicio/Categorias/page.jsx
+++ app/inicio/Categorias/page.jsx
@@ -1,32 +1,63 @@
 "use client";
-
 import { useState, useEffect } from "react";
-import { Space, Table, Button, Tag, Input, Form, message, Modal } from "antd";
+import {
+  Space,
+  Table,
+  Button,
+  Tag,
+  Input,
+  Form,
+  message,
+  Drawer,
+  Switch,
+  Popconfirm,
+} from "antd";
 import "./Categorias.css";
-import {useCategorias, crearCategoria, actualizarCategoria, eliminarCategoria} from "@/hooks/useCategorias";
+import { useCategorias } from "@/hooks/useCategorias";
 
-function EditCategoryModal({ visible, onClose, category, onSave }) {
+// Función para editar la categoría
+function EditCategoryDrawer({ visible, onClose, category, onSave }) {
   const [form] = Form.useForm();
+  const [isSaving, setIsSaving] = useState(false);
 
-  const handleSave = () => {
-    form.validateFields().then((values) => {
-      onSave(values);
-    });
+  useEffect(() => {
+    if (visible && category) {
+      form.setFieldsValue(category);
+    }
+  }, [category, form, visible]);
+
+  const handleSave = async () => {
+    try {
+      setIsSaving(true);
+      const values = await form.validateFields();
+      await onSave(values);
+      onClose();
+    } catch (error) {
+      console.error("Error al guardar la categoría:", error);
+    } finally {
+      setIsSaving(false);
+    }
   };
 
+  // Dawer para editar
   return (
-    <Modal
+    <Drawer
       title="Editar Categoría"
+      placement="right"
+      onClose={onClose}
       open={visible}
-      onCancel={onClose}
-      onOk={handleSave}
-    >
-      <Form
-        form={form}
-        initialValues={category}
-        layout="vertical"
-        onFinish={handleSave}
-      >
+      width={400}
+      forceRender={true}
+      styles={{
+        body: { padding: "24px", backgroundColor: "#fafafa" },
+        header: {
+          backgroundColor: "#f0f0f0",
+          borderBottom: "1px solid #e8e8e8",
+          fontWeight: "bold",
+        },
+      }}
+    > 
+      <Form form={form} layout="vertical" onFinish={handleSave}>
         <Form.Item
           name="nombre"
           label="Nombre"
@@ -39,154 +70,244 @@ function EditCategoryModal({ visible, onClose, category, onSave }) {
           label="Descripción"
           rules={[{ required: true, message: "Ingrese la descripción" }]}
         >
-          <Input.TextArea />
+          <Input.TextArea rows={6} />
         </Form.Item>
+        <Form.Item name="activo" label="Activo" valuePropName="checked">
+          <Switch />
+        </Form.Item>
+        <div className="flex justify-end mt-4">
+          <Button onClick={onClose} className="mr-2" disabled={isSaving}>
+            Cancelar
+          </Button>
+          <Button
+            type="primary"
+            htmlType="submit"
+            style={{ backgroundColor: "#FFA500", borderColor: "#FFA500" }}
+            loading={isSaving}
+          >
+            {isSaving ? "Guardando" : "Guardar"}
+          </Button>
+        </div>
       </Form>
-    </Modal>
+    </Drawer>
+  );
+}
+
+// Función para agregar categoría
+function AddCategoryDrawer({ visible, onClose, onAdd }) {
+  const [form] = Form.useForm();
+  const [isAdding, setIsAdding] = useState(false);
+
+  const handleAdd = async () => {
+    try {
+      setIsAdding(true);
+      const values = await form.validateFields();
+      await onAdd(values);
+      form.resetFields();
+      onClose();
+    } catch (error) {
+      console.error("Error al agregar la categoría:", error);
+    } finally {
+      setIsAdding(false);
+    }
+  };
+
+  //drawer para agregar
+  return (
+    <Drawer
+      title="Agregar Nueva Categoría"
+      placement="right"
+      onClose={onClose}
+      open={visible}
+      width={400}
+      styles={{
+        body: { padding: "24px", backgroundColor: "#fafafa" },
+        header: {
+          backgroundColor: "#f0f0f0",
+          borderBottom: "1px solid #e8e8e8",
+          fontWeight: "bold",
+        },
+      }}
+    >
+      <Form form={form} layout="vertical" onFinish={handleAdd}>
+        <Form.Item
+          name="nombre"
+          label="Nombre"
+          rules={[{ required: true, message: "Ingrese el nombre" }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name="descripcion"
+          label="Descripción"
+          rules={[{ required: true, message: "Ingrese la descripción" }]}
+        >
+          <Input.TextArea rows={6} />
+        </Form.Item>
+        <Form.Item name="activo" label="Activo" valuePropName="checked">
+          <Switch />
+        </Form.Item>
+        <div className="flex justify-end mb-2">
+          <Button onClick={onClose} className="mr-2" disabled={isAdding}>
+            Cancelar
+          </Button>
+          <Button type="primary" htmlType="submit" loading={isAdding}>
+            {isAdding ? "Agregando" : "Agregar"}
+          </Button>
+        </div>
+      </Form>
+    </Drawer>
   );
 }
 
 function Categorias() {
-    const { categorias, setCategorias, loading } = useCategorias();  // Usamos el hook para obtener las categorías y la función para actualizarlas
-    console.log("estos son las categorias",categorias);
-    const [searchTerm, setSearchTerm] = useState("");  // Nuevo estado para el filtro de búsqueda
-    const [filteredCategorias, setFilteredCategorias] = useState(categorias);  // Estado para las categorías filtradas
-  
-  
-
-  const [isModalVisible, setIsModalVisible] = useState(false);
+  const {
+    categorias,
+    loading,
+    crearCategoria,
+    actualizarCategoria,
+    deleteCategoria,
+  } = useCategorias();
+  const [searchTerm, setSearchTerm] = useState("");
+  const [isEditDrawerVisible, setIsEditDrawerVisible] = useState(false);
+  const [isAddDrawerVisible, setIsAddDrawerVisible] = useState(false);
   const [selectedCategory, setSelectedCategory] = useState(null);
 
-  const [form] = Form.useForm();
-
-  const handleAddCategory = (values) => {
-    const newCategory = {
-      id_categoria: categorias.length + 1,
-      nombre: values.nombre,
-      descripcion: values.descripcion,
-      activo: true,
-    };
-    setCategorias([...categorias, newCategory]);
-    form.resetFields();
-    message.success("Categoría agregada correctamente");
-  };
-
-  useEffect(() => {
-    // Actualizar el filtro cuando las categorias cambien
-    if (categorias) {
-      setFilteredCategorias(categorias);
-    }
-  }, [categorias]);
-
-  // Manejo del filtro de búsqueda
+  // buscar categoría
   const handleSearch = (e) => {
-    const value = e.target.value.toLowerCase();
-    setSearchTerm(value);  // Actualizamos el término de búsqueda
-
-    // Filtrar las categorías según el valor del término de búsqueda
-    const filtered = categorias.filter(
-      (item) =>
-        item.nombre.toLowerCase().includes(value) ||
-        item.descripcion.toLowerCase().includes(value)
-    );
-    setFilteredCategorias(filtered);  // Actualizamos las categorías filtradas
+    setSearchTerm(e.target.value.toLowerCase());
   };
 
+  const filteredCategorias = categorias?.filter(
+    (categoria) =>
+      categoria.nombre.toLowerCase().includes(searchTerm) ||
+      (categoria.descripcion &&
+        categoria.descripcion.toLowerCase().includes(searchTerm))
+  );
+
+  // editar categoría
   const handleEditCategory = (category) => {
     setSelectedCategory(category);
-    setIsModalVisible(true);
+    setIsEditDrawerVisible(true);
   };
 
-  const handleSaveCategory = (values) => {
-    const updatedCategorias = categorias.map((cat) =>
-      cat.id_categoria === selectedCategory.id_categoria
-        ? { ...cat, ...values }
-        : cat
-    );
-    setCategorias(updatedCategorias);
-    setIsModalVisible(false);
+  const handleSaveCategory = async (values) => {
+    await actualizarCategoria(selectedCategory.id_categoria, values);
     message.success("Categoría actualizada correctamente");
+    setIsEditDrawerVisible(false);
   };
 
+  // agregar categoría
+  const handleAddCategory = async (values) => {
+    await crearCategoria(values);
+    message.success("Categoría agregada correctamente");
+    setIsAddDrawerVisible(false);
+  };
+
+  // Función para eliminar la categoría
+  const handleDeleteCategory = async (id) => {
+    try {
+      await deleteCategoria(id);
+      message.success("Categoría eliminada correctamente");
+    } catch (error) {
+      message.error("Error al eliminar la categoría");
+    }
+  };
+
+  // tabla de categorías
   const columns = [
-    {
-      title: "Nombre",
-      dataIndex: "nombre",
-      key: "nombre",
-      width: 150, // Establece un ancho fijo para la columna
-    },
+    { title: "Nombre", dataIndex: "nombre", key: "nombre", width: 150 },
     {
       title: "Descripción",
       dataIndex: "descripcion",
       key: "descripcion",
-      width: 300, // Establece un ancho fijo para la columna
+      width: 300,
     },
     {
       title: "Estado",
       dataIndex: "activo",
       key: "activo",
       render: (activo) => (
-        <Tag color={activo ? "green" : "red"}>{activo ? "Activo" : "Inactivo"}</Tag>
+        <Tag
+          style={{
+            backgroundColor: activo ? "#11b016" : "#808080",
+            color: "white",
+          }}
+        >
+          {activo ? "Activo" : "Inactivo"}
+        </Tag>
       ),
-      width: 100, // Establece un ancho fijo para la columna
+      width: 100,
     },
     {
       title: "Acciones",
       key: "acciones",
       render: (_, record) => (
         <Space size="middle">
-          <Button onClick={() => handleEditCategory(record)}>Editar</Button>
-          <Button danger>Eliminar</Button>
+          <Button
+            onClick={() => handleEditCategory(record)}
+            className="btn-editar"
+          >
+            Editar
+          </Button>
+          <Popconfirm
+            title="¿Está seguro de eliminar esta categoría?"
+            onConfirm={() => handleDeleteCategory(record.id_categoria)}
+            okText="Sí"
+            cancelText="No"
+          >
+            <Button className="btn-eliminar" danger>
+              Eliminar
+            </Button>
+          </Popconfirm>
         </Space>
       ),
-      width: 150, // Establece un ancho fijo para la columna
+      width: 150,
     },
   ];
-  
 
   return (
     <>
-      <div className="contenedor">
-        <div className="titulo">
-          <input
+      <div
+        className="table-container bg-gradient-to-r from-blue-100 to-blue-50 rounded-xl shadow-md hover:shadow-xl p-6 w-full max-w-[1400px] h-[600px] 
+      overflow-y-auto transform hover:scale-105 transition-all duration-300 border border-gray-300"
+      >
+        <h1 className="text-3xl font-bold text-gray-800 mb-10">Categorías</h1>
+        <div className="titulo flex items-center justify-between mt-5 mb-4">
+          <Input
             type="text"
-            placeholder="Buscar categoría"
-            className="busqueda"
-            value={searchTerm}  // Vinculamos el input al estado searchTerm
-            onChange={handleSearch}  // Llamamos al filtro de búsqueda
+            placeholder="Buscar por nombre"
+            className="busqueda w-1/4 p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 mr-2"
+            value={searchTerm}
+            onChange={handleSearch}
           />
+          <Button type="primary" onClick={() => setIsAddDrawerVisible(true)}>
+            Agregar Categoría
+          </Button>
         </div>
         <div className="table-container">
           <Table
-            dataSource={filteredCategorias}  // Usamos las categorías filtradas
+            dataSource={filteredCategorias}
             columns={columns}
             rowKey="id_categoria"
             loading={loading}
-            pagination={{ pageSize: 4 }}
+            pagination={{ pageSize: 6 }}
+            className="custom-table"
           />
-        </div>
-        <div className="form-container">
-          <Form className="formulario" form={form} onFinish={handleAddCategory} layout="vertical">
-            <h3>Agregar Categoría</h3>
-            <Form.Item name="nombre" label="Nombre" rules={[{ required: true, message: "Ingrese el nombre" }]}>
-              <Input />
-            </Form.Item>
-            <Form.Item name="descripcion" label="Descripción" rules={[{ required: true, message: "Ingrese la descripción" }]}>
-              <Input.TextArea />
-            </Form.Item>
-            <Form.Item>
-              <Button type="primary" htmlType="submit">Agregar</Button>
-            </Form.Item>
-          </Form>
         </div>
       </div>
 
-      {/* Modal Edit */}
-      <EditCategoryModal
-        visible={isModalVisible}
-        onClose={() => setIsModalVisible(false)}
+      <EditCategoryDrawer
+        visible={isEditDrawerVisible}
+        onClose={() => setIsEditDrawerVisible(false)}
         category={selectedCategory}
         onSave={handleSaveCategory}
+      />
+
+      <AddCategoryDrawer
+        visible={isAddDrawerVisible}
+        onClose={() => setIsAddDrawerVisible(false)}
+        onAdd={handleAddCategory}
       />
     </>
   );
--- hooks/useCategorias.ts
+++ hooks/useCategorias.ts
@@ -9,13 +9,11 @@ interface Categoria {
   fecha_creacion: string;
 }
 
-//Obtiene todas las categorias
 export function useCategorias() {
-  const [categorias, setCategorias] = useState<Categoria[] | null>(null);  // Cambiado 'data' por 'categorias'
+  const [categorias, setCategorias] = useState<Categoria[] | null>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
-  // Cargar todas las categorías
   useEffect(() => {
     async function fetchCategorias() {
       try {
@@ -23,13 +21,14 @@ export function useCategorias() {
         if (!response.ok) throw new Error("Error al obtener los datos");
 
         const result = await response.json();
-        //console.log(result);
-
-        setCategorias(result.map((categoria: any) => ({
-          ...categoria,
-          descripcion: categoria.descripcion || null,
-          fecha_creacion: categoria.fecha_creacion || new Date().toISOString()
-        })));
+        setCategorias(
+          result.map((categoria: any) => ({
+            ...categoria,
+            descripcion: categoria.descripcion || null,
+            fecha_creacion:
+              categoria.fecha_creacion || new Date().toISOString(),
+          }))
+        );
       } catch (err: any) {
         setError(err.message);
       } finally {
@@ -40,62 +39,116 @@ export function useCategorias() {
     fetchCategorias();
   }, []);
 
-  // Crear una nueva categoría
-  async function crearCategoria(nuevaCategoria: Omit<Categoria, 'id_categoria' | 'fecha_creacion'>) {
+  // Función para crea una categoría
+  async function crearCategoria(
+    nuevaCategoria: Omit<Categoria, "id_categoria" | "fecha_creacion">
+  ) {
     try {
-      const response = await fetch("/Api/categorias", {
+      const response = await fetch("/Api/categorias/crearNuevo", {
         method: "POST",
         headers: {
-          "Content-Type": "application/json"
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify(nuevaCategoria)
+        body: JSON.stringify(nuevaCategoria),
       });
 
       if (!response.ok) throw new Error("Error al crear la categoría");
 
       const createdCategoria = await response.json();
-      setCategorias((prevCategorias) => prevCategorias ? [...prevCategorias, createdCategoria] : [createdCategoria]);
+      setCategorias((prevCategorias) =>
+        prevCategorias
+          ? [...prevCategorias, createdCategoria[0]]
+          : [createdCategoria[0]]
+      );
+      return createdCategoria[0];
     } catch (err: any) {
       setError(err.message);
+      throw err;
     }
   }
 
-  // Actualizar una categoría
-  async function actualizarCategoria(id: number, categoriaActualizada: Partial<Categoria>) {
+  // Función para editar una categoría
+  async function actualizarCategoria(
+    id: number,
+    categoriaActualizada: Partial<Categoria>
+  ) {
     try {
       const response = await fetch(`/Api/categorias/editar/${id}`, {
         method: "PUT",
         headers: {
-          "Content-Type": "application/json"
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify(categoriaActualizada)
+        body: JSON.stringify(categoriaActualizada),
       });
 
       if (!response.ok) throw new Error("Error al actualizar la categoría");
 
       const updatedCategoria = await response.json();
-      setCategorias((prevCategorias) => prevCategorias ? prevCategorias.map((categoria) => 
-        categoria.id_categoria === id ? updatedCategoria : categoria
-      ) : null);
+
+      setCategorias((prevCategorias) =>
+        prevCategorias
+          ? prevCategorias.map((categoria) =>
+              categoria.id_categoria === id ? updatedCategoria[0] : categoria
+            )
+          : null
+      );
+      return updatedCategoria[0];
     } catch (err: any) {
       setError(err.message);
+      throw err;
     }
   }
 
-  // Eliminar una categoría
-  async function eliminarCategoria(id: number) {
+  // Función para eliminar una categoría
+  async function deleteCategoria(id: number) {
     try {
-      const response = await fetch(`/Api/categorias/${id}`, {
+      const response = await fetch(`/Api/categorias/eliminar/${id}`, {
         method: "DELETE",
       });
-
-      if (!response.ok) throw new Error("Error al eliminar la categoría");
-
-      setCategorias((prevCategorias) => prevCategorias ? prevCategorias.filter((categoria) => categoria.id_categoria !== id) : null);
+      if (!response.ok) {
+        const data = await response.json();
+        throw new Error(data.error || "Error al eliminar la categoría");
+      }
+      setCategorias((prevCategorias) =>
+        prevCategorias
+          ? prevCategorias.filter((cat) => cat.id_categoria !== id)
+          : null
+      );
+      const result = await response.json();
+      return result;
     } catch (err: any) {
       setError(err.message);
+      throw err;
     }
   }
 
-  return { categorias, loading, error, crearCategoria, actualizarCategoria, eliminarCategoria }; 
+  // Función para buscar una categoría por nombre
+  async function searchCategoria(nombre: string): Promise<Categoria> {
+    if (!nombre.trim()) {
+      throw new Error("El parámetro 'nombre' es requerido");
+    }
+    try {
+      const response = await fetch(
+        `/Api/categorias/buscarPorNombre?nombre=${encodeURIComponent(nombre)}`
+      );
+      if (!response.ok) {
+        const data = await response.json();
+        throw new Error(data.error || `Error HTTP: ${response.status}`);
+      }
+      const result: Categoria = await response.json();
+      return result;
+    } catch (err: any) {
+      throw new Error(err.message || "Error al buscar la categoría");
+    }
+  }
+
+  return {
+    categorias,
+    loading,
+    error,
+    crearCategoria,
+    actualizarCategoria,
+    deleteCategoria,
+    searchCategoria,
+  };
 }
