ad176c6a210485c869a22d6ed5281484392c87b9
JackeVillalobos <jackelinevillalobos11@gmail.com>
2025-02-23 00:43:06 -0600

    Actualiza estilos del login y registro

--- app/(auth-pages)/sign-in/page.tsx
+++ app/(auth-pages)/sign-in/page.tsx
@@ -4,53 +4,93 @@ import { SubmitButton } from "@/components/submit-button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
+import { MailOutlined, LockOutlined } from "@ant-design/icons";
 
 export default async function Login(props: { searchParams: Promise<Message> }) {
   const searchParams = await props.searchParams;
   return (
-    <div className="flex items-center justify-center min-h-[80vh] mx-4">
+    <div className="flex items-center justify-center min-h-[90vh] w-full">
+      <div className="w-full sm:max-w-2xl lg:w-[450px] bg-white dark:bg-gray-900 p-8 rounded-[12px] shadow-md border border-gray-200 relative">
+        {/* Logo */}
+        <div
+          className="absolute top-[-40px] left-1/2 transform -translate-x-1/2 w-32 h-32 bg-white dark:bg-gray-900 rounded-full shadow-md flex 
+        items-center justify-center border border-gray-300 gap-4"
+        >
+          <img
+            src="/img/taller-logo.png"
+            alt="Logo del taller"
+            className="w-full h-full rounded-full"
+          />
+        </div>
 
-
-      <form className="w-full max-w-sm bg-white dark:bg-gray-900 p-6 rounded-lg shadow-md">
-        <h1 className="text-2xl font-semibold text-center text-gray-900 dark:text-white">
-          Iniciar Sesión
-        </h1>
-        <p className="text-sm text-gray-600 dark:text-gray-300 text-center mt-2">
-          No tienes una cuenta?{" "}
-          <Link
-            className="text-blue-500 dark:text-blue-400 font-medium underline"
-            href="/sign-up"
-          >
-            Regístrate
-          </Link>
-        </p>
-        <div className="flex flex-col gap-3 mt-6">
-          <Label htmlFor="email" className="text-gray-700 dark:text-gray-300">
-            Correo
-          </Label>
-          <Input name="email" placeholder="you@example.com" required />
-
-          <div className="flex justify-between items-center">
-            <Label htmlFor="password" className="text-gray-700 dark:text-gray-300">
-              Contraseña
+        <form className="flex flex-col mt-20 gap-1">
+          <h1 className="text-2xl font-semibold text-center text-gray-900 dark:text-white">
+            Iniciar Sesión
+          </h1>
+          <p className="text-sm text-gray-600 dark:text-gray-300 text-center mt-2">
+            ¿Aún no tienes una cuenta?{" "}
+            <Link
+              className="text-blue-500 dark:text-blue-500 font-medium hover:text-blue-700 dark:hover:text-blue-300 focus:underline focus:outline-none 
+              focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 p-1 transition duration-300 ease-in-out"
+              href="/sign-up"
+            >
+              Regístrate
+            </Link>
+          </p>
+          <div className="flex flex-col gap-3 mt-3">
+            <Label htmlFor="email" className="text-gray-700 dark:text-gray-300">
+              Correo
             </Label>
-            
-          </div>
-          <Input type="password" name="password" placeholder="Tu contraseña" required />
+            <div className="relative">
+              <MailOutlined className="absolute left-4 top-3 text-gray-500 dark:text-gray-400" />
+              <Input
+                name="email"
+                placeholder="you@example.com"
+                required
+                className="w-full pl-10 py-3 border border-blue-200 rounded-[12px] bg-blue-100 dark:bg-gray-800 dark:border-gray-700
+                focus:outline-none focus:ring-4 focus:ring-blue-400 dark:focus:ring-blue-300 focus:border-blue-500 placeholder-gray-500 
+                dark:placeholder-gray-400 placeholder-opacity-75 shadow-sm hover:shadow-md transition"
+              />
+            </div>
 
-          <SubmitButton pendingText="Iniciando sesión..." formAction={signInAction}
-          className="bg-blue-500 text-white font-semibold py-2 px-4 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300">
-            Iniciar sesión
-          </SubmitButton>
-          <FormMessage message={searchParams} />
-          <Link
+            <div className="flex justify-between items-center">
+              <Label
+                htmlFor="password"
+                className="mt-2 text-gray-700 dark:text-gray-300"
+              >
+                Contraseña
+              </Label>
+            </div>
+            <div className="relative">
+              <LockOutlined className="absolute left-4 top-3 text-gray-500 dark:text-gray-400" />
+              <Input
+                type="password"
+                name="password"
+                placeholder="Tu contraseña"
+                required
+                className="w-full pl-10 py-3 border border-blue-200 rounded-[12px] bg-blue-100 dark:bg-gray-800 dark:border-gray-700
+                focus:outline-none focus:ring-4 focus:ring-blue-400 dark:focus:ring-blue-300 focus:border-blue-500 placeholder-gray-500 
+                dark:placeholder-gray-400 placeholder-opacity-75 shadow-sm hover:shadow-md transition"
+              />
+            </div>
+
+            <SubmitButton
+              pendingText="Iniciando sesión..."
+              formAction={signInAction}
+              className="mt-4 bg-blue-500 text-white font-semibold py-2 px-4 rounded-[12px] hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300"
+            >
+              Iniciar sesión
+            </SubmitButton>
+            <FormMessage message={searchParams} />
+            <Link
               className="text-xs text-blue-500 dark:text-blue-400 underline flex justify-center"
               href="/forgot-password"
             >
               ¿Olvidaste tu contraseña?
             </Link>
-        </div>
-      </form>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
--- app/(auth-pages)/sign-up/page.tsx
+++ app/(auth-pages)/sign-up/page.tsx
@@ -4,9 +4,12 @@ import { SubmitButton } from "@/components/submit-button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
+import { MailOutlined, LockOutlined } from "@ant-design/icons"; // Importar los iconos
 import { SmtpMessage } from "../smtp-message";
 
-export default async function Signup(props: { searchParams: Promise<Message> }) {
+export default async function Signup(props: {
+  searchParams: Promise<Message>;
+}) {
   const searchParams = await props.searchParams;
 
   if ("message" in searchParams) {
@@ -18,43 +21,80 @@ export default async function Signup(props: { searchParams: Promise<Message> }) 
   }
 
   return (
-    <div className="flex items-center justify-center min-h-[80vh] mx-4">
-
-      <div className="w-full max-w-sm bg-white dark:bg-gray-900 p-6 rounded-lg shadow-md">
-        <h1 className="text-2xl font-semibold text-center text-gray-900 dark:text-white">
+    <div className="flex items-center justify-center min-h-[90vh] w-full">
+      <div className="w-full sm:max-w-2xl lg:w-[450px] bg-white dark:bg-gray-900 p-8 rounded-[12px] shadow-md border border-gray-200 relative">
+        {/* Logo */}
+        <div
+          className="absolute top-[-40px] left-1/2 transform -translate-x-1/2 w-32 h-32 bg-white dark:bg-gray-900 rounded-full 
+        shadow-md flex items-center justify-center border border-gray-300 gap-4"
+        >
+          <img
+            src="/img/taller-logo.png"
+            alt="Logo del taller"
+            className="w-full h-full rounded-full"
+          />
+        </div>
+        <h1 className="text-3xl font-semibold text-center text-gray-900 dark:text-white mt-20">
           Registro
         </h1>
         <p className="text-sm text-gray-600 dark:text-gray-300 text-center mt-2">
           ¿Ya tienes una cuenta?{" "}
-          <Link className="text-blue-500 dark:text-blue-700 font-medium underline" href="/sign-in">
+          <Link
+            className="text-blue-500 dark:text-blue-700 font-medium hover:text-blue-700 dark:hover:text-blue-500 focus:underline 
+          focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 p-1 transition duration-300 ease-in-out"
+            href="/sign-in"
+          >
             Inicia sesión
           </Link>
         </p>
-        <form className="flex flex-col gap-3 mt-6">
-          <Label htmlFor="email" className="text-gray-700 dark:text-gray-300">
+        <form className="flex flex-col gap-1">
+          <label
+            htmlFor="email"
+            className="mt-4 text-gray-700 dark:text-gray-300"
+          >
             Correo
-          </Label>
-          <Input name="email" placeholder="you@example.com" required />
-
-          <Label htmlFor="password" className="text-gray-700 dark:text-gray-300">
+          </label>
+          <div className="relative">
+            <MailOutlined className="absolute left-4 top-3 text-gray-500 dark:text-gray-400" />
+            <Input
+              name="email"
+              placeholder="you@example.com"
+              required
+              className="w-full pl-10 py-3 border border-blue-200 rounded-[12px] bg-blue-100 dark:bg-gray-800 dark:border-gray-700
+                focus:outline-none focus:ring-4 focus:ring-blue-400 dark:focus:ring-blue-300 focus:border-blue-500 placeholder-gray-500 
+                dark:placeholder-gray-400 placeholder-opacity-75 shadow-sm hover:shadow-md transition"
+            />
+          </div>
+          <label
+            htmlFor="password"
+            className="mt-4 text-gray-700 dark:text-gray-300"
+          >
             Contraseña
-          </Label>
-          <Input
-            type="password"
-            name="password"
-            placeholder="Tu contraseña"
-            minLength={6}
-            required
-          />
+          </label>
+          <div className="relative">
+            <LockOutlined className="absolute left-4 top-3 text-gray-500 dark:text-gray-400" />
+            <Input
+              type="password"
+              name="password"
+              placeholder="Tu contraseña"
+              minLength={6}
+              required
+              className="w-full pl-10 py-3 border border-blue-200 rounded-[12px] bg-blue-100 dark:bg-gray-800 dark:border-gray-700
+                focus:outline-none focus:ring-4 focus:ring-blue-400 dark:focus:ring-blue-300 focus:border-blue-500 placeholder-gray-500 
+                dark:placeholder-gray-400 placeholder-opacity-75 shadow-sm hover:shadow-md transition"
+            />
+          </div>
 
-          <SubmitButton formAction={signUpAction} pendingText="Registrando..."
-            className="bg-blue-500 text-white font-semibold py-2 px-4 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300">
+          <SubmitButton
+            formAction={signUpAction}
+            pendingText="Registrando..."
+            className="mt-5 bg-blue-500 text-white font-semibold py-3 px-6 rounded-[12px] hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300 w-full"
+          >
             Registrarse
           </SubmitButton>
           <FormMessage message={searchParams} />
         </form>
       </div>
-
     </div>
   );
 }
