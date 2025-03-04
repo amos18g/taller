74a37100ab1e20ac96b0813aaed90c1ef0129dc2
amos18g <so.erick.tereo@gmail.com>
2025-03-03 21:49:58 -0600

    se agrego backend para obtener todas las compras

---
+++ app/Api/compras/obtenerTodos/route.tsx
@@ -0,0 +1,18 @@
+// Api/compras/obtenerTodos
+
+import { NextResponse } from "next/server";
+import { createClient } from "@/utils/supabase/server";
+
+export async function GET() {
+  const supabase = await createClient();
+
+  const { data, error } = await supabase
+    .from("compras")
+    .select("*");
+
+  if (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+
+  return NextResponse.json(data, { status: 200 });
+}
