8351f2ea03c6b81d947c56f56b82b4e756ed4d55
jdg03 <gonzalez5.jose52@gmail.com>
2025-03-04 08:41:49 -0600

    buscar compra por id

---
+++ app/Api/compras/[id]/route.tsx
@@ -0,0 +1,24 @@
+// Api/compras/id
+
+import { NextResponse, NextRequest } from "next/server";
+import { createClient } from "@/utils/supabase/server";
+
+export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
+  const supabase = await createClient();
+
+  const id = params.id;
+  console.log("El id de la factura es:", id);
+
+
+
+  const { data, error } = await supabase
+    .from("compras")
+    .select("*")
+    .eq("id_historial", id);
+
+  if (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+
+  return NextResponse.json(data, { status: 200 });
+}
