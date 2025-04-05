f82445ed444895501d7c30a6b9bb0fdb23ae1f96
José David Gonzalez <gonzalez5.jose52@gmail.com>
2025-04-05 13:23:33 -0600

    hook usuarios

---
+++ hooks/useUsuarios.ts
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+
+interface Usuario {
+  id: string;
+  name: string;
+  email: string;
+  role: string;
+
+}
+
+interface UseUsuariosResult {
+  usuarios: Usuario[] | null;
+  loading: boolean;
+  error: string | null;
+}
+
+export const useUsuarios = (userId: string | null): UseUsuariosResult => {
+  const [usuarios, setUsuarios] = useState<Usuario[] | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (!userId) return;
+
+    const fetchUsuarios = async () => {
+      setLoading(true);
+      setError(null);
+
+      try {
+        const res = await fetch(`/api/usuarios/getUsuarios?id=${userId}`);
+
+        if (!res.ok) {
+          const data = await res.json();
+          throw new Error(data.error || "Error al obtener los usuarios");
+        }
+
+        const data = await res.json();
+        setUsuarios(data);
+      } catch (err: any) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUsuarios();
+  }, [userId]);
+
+  return { usuarios, loading, error };
+};
