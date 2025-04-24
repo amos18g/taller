7d2cfe0d704f5ab0327c2873d47ef8a9e9b5f561
José David Gonzalez <gonzalez5.jose52@gmail.com>
2025-04-23 23:41:23 -0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  devIndicators: {
+    // Elimina totalmente el indicador
+    buildActivity: false,
+
+    // (opcional) Si solo quieres cambiar de esquina:
+    // buildActivityPosition: 'bottom-right', // admite 'bottom-right' | 'bottom-left' | 'top-right' | 'top-left'
+  },
 };
 
 export default nextConfig;
